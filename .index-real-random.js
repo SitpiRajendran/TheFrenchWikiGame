bb7fff405756f6afc87a63fc25c7b501a6d04d73
Sitpi Rajendran <sitpi-kevin.rajendran@epitech.eu>
2021-03-28 01:55:53 +0100

    [ADD] V1

---
+++ .index-real-random.js
@@ -0,0 +1,62 @@
+var fetch = require("node-fetch");
+
+async function searchinWikipedia() {
+    var article_depart = {
+        pageid: undefined,
+        title: undefined,
+        canonical: undefined,
+        thumbnail: undefined,
+        url: undefined,
+        description: undefined,
+        next: undefined,
+    }
+
+    var article_fin = {
+        pageid: undefined,
+        title: undefined,
+        canonical: undefined,
+        thumbnail: undefined,
+        description: undefined,
+        next: undefined,
+    }
+
+    var url = "https://fr.wikipedia.org/api/rest_v1/page/random/summary"
+
+    await fetch(url).then(async function (response) {
+        if (response.status == 200)
+            return response.json()
+        else {
+            console.log(response)
+            await delay(2000);
+            console.log("OK BRO FIRST")
+        }
+    })
+        .then(function (response) {
+            article_depart.pageid = response.pageid;
+            article_depart.title = response.title;
+            article_depart.canonical = response.titles.canonical;
+            article_depart.url = response.content_urls.desktop.page;
+            article_depart.thumbnail = response.thumbnail.source;
+            article_depart.description = response.description;
+            console.log(article_depart)
+        });
+
+}
+
+searchinWikipedia();
+
+// BACK LINKS
+
+/* var backlinks = "https://fr.wikipedia.org/w/api.php?action=query&list=backlinks&blpageid=" + article_fin.pageid + "&bllimit=max&format=json";
+
+await fetch(backlinks).then(async function (response) {
+    if (response.status == 200)
+        return response.json()
+    else {
+        console.log(response)
+        await delay(2000);
+        console.log("OK BRO BACKLINKS")
+    }
+}).then(function(response) {
+    console.log(response)
+}) */
--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-var fetch = require("node-fetch");
+const indexRouter = require('./routes/index')
+var session = require("express-session");
+var bodyParser = require("body-parser");
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+// EXPRESS SETUP
+const express = require('express')
+const app = express()
 
-async function searchinWikipedia() {
-    var item = 0;
-    var article_depart = {
-        pageid: undefined,
-        title: undefined,
-        thumbnail: undefined,
-        description: undefined,
-        next: undefined,
-    }
-    var url = "https://fr.wikipedia.org/api/rest_v1/page/random/summary"
-    await fetch(url).then(function (response) { return response.json(); })
-        .then(function (response) {
-            article_depart.pageid = response.pageid;
-            article_depart.title = response.title;
-            article_depart.thumbnail = response.thumbnail.source;
-            article_depart.description = response.description;
-        });
-    console.log("START : " + article_depart.title);
-    
-    var next_article = article_depart.title
-    while (item != (Math.random() * (12 - 2) - 3)) {
-        await fetch("https://fr.wikipedia.org/w/api.php?action=query&titles=" + next_article + "&prop=links&pllimit=max&format=json").then(function (response) { return response.json(); })
-            .then(function (response) {
-                var link_list = Object.values(response.query.pages)[0].links
-                var random = parseInt(link_list.length * Math.random());
-                next_article = link_list[random].title
-                console.log(next_article);
-            });
-        item++;
-        await delay(2000);
-    }
-}
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.use(express.static(__dirname + '/public'))
+app.use('/models', express.static(__dirname + '/models'))
+app.use(express.static("public"));
+app.use('/img', express.static(__dirname + 'public/img'))
 
-searchinWikipedia();
+// ROUTER SETUP
+app.use('/', indexRouter)
+
+app.get('*', function(req, res, next){
+    res.status(404);
+    if (req.accepts('html')) {
+      res.redirect('https://sitpi.pro');
+      return;
+    }  
+    res.type('txt').send('Not found');
+  });
+
+app.listen(process.env.PORT || 80)
